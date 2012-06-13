56b3d42ecbce57d24ec4514e5bce76a8f87f0ac9
Alex GUO <supereic@gmail.com>
2012-06-14 00:20:22 +0800

    * start alu project

---
+++ prj-120618/rtl/verilog/alu.v
@@ -0,0 +1,17 @@
+module alu(
+
+input 		mode_control_i,
+input	[3:0]	select_input_i,
+
+input	[3:0]	operand_a_i,
+input	[3:0]	operand_b_i,
+input		carry_input_i,
+
+output	[3:0]	function_output_o,
+output		generate_output_o,
+output		propagate_output_o,
+output		carry_output_o,
+output		cmp_output_o
+
+);
+endmodule
